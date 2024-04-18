4d51dedf7e653a0dc9ec0751f77bf3fda3b939ff
chambaz <adammchambers@gmail.com>
2024-04-18 17:50:44 -0400

    feat: add account button / wallet auth accounts state

--- apps/marginfi-v2-ui/src/components/common/Wallet/components/Authentication/WalletAuthAccounts.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/components/Authentication/WalletAuthAccounts.tsx
@@ -3,13 +3,24 @@ import React from "react";
 import { useMrgnlendStore } from "~/store";
 
 import { Button } from "~/components/ui/button";
-import { IconChevronDown } from "~/components/ui/icons";
+import { IconChevronDown, IconUserPlus } from "~/components/ui/icons";
 import { Label } from "~/components/ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
 import { shortenAddress } from "@mrgnlabs/mrgn-common";
 import { Badge } from "~/components/ui/badge";
+import { Input } from "~/components/ui/input";
+
+enum WalletAuthAccountsState {
+  DEFAULT = "DEFAULT",
+  ADD_ACCOUNT = "ADD_ACCOUNT",
+  EDIT_ACCOUNT = "EDIT_ACCOUNT",
+}
 
 export const WalletAuthAccounts = () => {
+  const [walletAuthAccountsState, setWalletAuthAccountsState] = React.useState<WalletAuthAccountsState>(
+    WalletAuthAccountsState.DEFAULT
+  );
+  const [newAccountName, setNewAccountName] = React.useState<string>("");
   const [initialized, marginfiAccounts, selectedAccount, fetchMrgnlendState] = useMrgnlendStore((state) => [
     state.initialized,
     state.marginfiAccounts,
@@ -22,6 +33,11 @@ export const WalletAuthAccounts = () => {
     const index = marginfiAccounts.findIndex((account) => account.address.equals(selectedAccount.address));
     return `Account ${index + 1}`;
   }, [selectedAccount, marginfiAccounts]);
+
+  React.useEffect(() => {
+    if (!marginfiAccounts.length) return;
+    setNewAccountName(`Account ${marginfiAccounts.length + 1}`);
+  }, [marginfiAccounts.length]);
 
   if (!initialized || !marginfiAccounts.length) return null;
 
@@ -37,29 +53,73 @@ export const WalletAuthAccounts = () => {
         )}
         {/* TODO: fix this z-index mess */}
         <PopoverContent className="w-80 z-[9999999]">
-          <div className="grid gap-4">
-            <div className="space-y-2">
-              <h4 className="font-medium leading-none">Your accounts</h4>
-              <p className="text-sm text-muted-foreground">Select your marginfi account below.</p>
+          {walletAuthAccountsState === WalletAuthAccountsState.DEFAULT && (
+            <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none">Your accounts</h4>
+                <p className="text-sm text-muted-foreground">Select your marginfi account below.</p>
+              </div>
+              <div className="grid gap-2">
+                {marginfiAccounts.map((account, index) => (
+                  <Button
+                    key={index}
+                    variant="ghost"
+                    className="justify-start gap-4 px-2"
+                    onClick={() => {
+                      localStorage.setItem("mfiAccount", account.address.toBase58());
+                      fetchMrgnlendState();
+                    }}
+                  >
+                    <Label htmlFor="width">Account {index + 1}</Label>
+                    <span className="text-muted-foreground">{shortenAddress(account.address.toBase58())}</span>
+                    {selectedAccount && selectedAccount.address.equals(account.address) && <Badge>active</Badge>}
+                  </Button>
+                ))}
+              </div>
+              <Button
+                variant="outline"
+                className="w-full"
+                onClick={() => setWalletAuthAccountsState(WalletAuthAccountsState.ADD_ACCOUNT)}
+              >
+                <IconUserPlus size={16} className="mr-2" />
+                Add account
+              </Button>
             </div>
-            <div className="grid gap-2">
-              {marginfiAccounts.map((account, index) => (
-                <Button
-                  key={index}
-                  variant="ghost"
-                  className="justify-start gap-4 px-2"
-                  onClick={() => {
-                    localStorage.setItem("mfiAccount", account.address.toBase58());
-                    fetchMrgnlendState();
-                  }}
-                >
-                  <Label htmlFor="width">Account {index + 1}</Label>
-                  <span className="text-muted-foreground">{shortenAddress(account.address.toBase58())}</span>
-                  {selectedAccount && selectedAccount.address.equals(account.address) && <Badge>active</Badge>}
-                </Button>
-              ))}
+          )}
+
+          {walletAuthAccountsState === WalletAuthAccountsState.ADD_ACCOUNT && (
+            <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none">Your accounts</h4>
+                <p className="text-sm text-muted-foreground">Create a new marginfi account.</p>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="accountName" className="font-medium">
+                  Account name
+                </Label>
+                <Input
+                  type="text"
+                  name="accountName"
+                  value={newAccountName}
+                  autoFocus
+                  onChange={(e) => setNewAccountName(e.target.value)}
+                />
+              </div>
+              <Button className="w-full" onClick={() => {}}>
+                Create account
+              </Button>
+              <Button
+                variant="link"
+                size="sm"
+                className="text-destructive-foreground h-5"
+                onClick={() => {
+                  setWalletAuthAccountsState(WalletAuthAccountsState.DEFAULT);
+                }}
+              >
+                Cancel
+              </Button>
             </div>
-          </div>
+          )}
         </PopoverContent>
       </Popover>
     </div>
